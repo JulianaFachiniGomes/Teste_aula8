53bafa139c9565db955c029674baba0ce10d1731
JulianaFachiniGomes <jubfachini@gmail.com>
2024-11-13 11:06:23 -0300

    Update teste_salinha.py

--- teste_salinha.py
+++ teste_salinha.py
@@ -4,4 +4,11 @@ st.write('Oi')
 st.write('estou aprendendo')
 st.write('acho que entendi!')
 
+st.title("Este é o título do app")
+st.header("Este é o subtítulo")
+st.subheader("Este é o terceiro subtítulo")
+st.markdown("Este é texto")
+st.caption("Esta é a a legenda")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
