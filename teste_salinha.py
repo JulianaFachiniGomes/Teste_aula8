af95a9227f30e1fe2150afe40e1a4df71728336b
JulianaFachiniGomes <jubfachini@gmail.com>
2024-11-13 11:18:12 -0300

    Update teste_salinha.py

--- teste_salinha.py
+++ teste_salinha.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 st.write('Oi')
 
@@ -14,3 +15,4 @@ st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
 st.slider('Selecione uma respota', 0,100)
 st.select_slider('Selecione uma resposta', options=['A', 'B', 'C'])
+st.select_slider('Selecione uma resposta', ['A', 'B', 'C'])
